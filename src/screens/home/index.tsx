b91c0814e5078719705251d2e47b49628d84ddc9
kashifs <kashifs@valuefy.com>
2024-06-24 11:42:28 +0530

    Button displayed at bottom of dashboard

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -4,14 +4,18 @@ import styles from './styles';
 import images from '../../assets/images';
 import HighlightContainer from './components/HighlightContainer';
 import CategoryContainer from './components/CategoryContainer';
+import Button from '../../components/Button';
 
 const Home = () => {
   return (
-    <ScrollView style={styles.container}>
-      <Image source={images.welcome} style={styles.image} />
-      <HighlightContainer />
-      <CategoryContainer />
-    </ScrollView>
+    <>
+      <ScrollView style={styles.container}>
+        <Image source={images.welcome} style={styles.image} />
+        <HighlightContainer />
+        <CategoryContainer />
+      </ScrollView>
+      <Button label="Book a trip" addMarginHorizontal showAsBottomAbsolute />
+    </>
   );
 };
 
