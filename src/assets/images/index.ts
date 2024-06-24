4b5cd4221a63ed23503fab01cd53bf01756934f5
kashifs <kashifs@valuefy.com>
2024-06-24 11:15:18 +0530

    images added

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,5 +1,10 @@
 const images = {
   aloha: require('./Aloha.png'),
+  welcome: require('./Welcome.png'),
+  surfing: require('./Surfing.png'),
+  hula: require('./Hula.png'),
+  vulcanoes: require('./Vulcanoes.png'),
+  arrowRight: require('./ArrowRight.png'),
 };
 
 export default images;
